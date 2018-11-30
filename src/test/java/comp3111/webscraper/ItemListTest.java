7ea1874ba4ded25bb0e166a9fcf894777a0ba815
BoscoTin <tintin111198@gmail.com>
2018-11-30 23:34:32 +0800

    Update unit test with 100% and coverage up to 37%

--- src/test/java/comp3111/webscraper/ItemListTest.java
+++ src/test/java/comp3111/webscraper/ItemListTest.java
@@ -4,36 +4,38 @@ package comp3111.webscraper;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.util.List;
 import java.util.Vector;
 
 
 public class ItemListTest {
-	private ItemList setItemList() {
+	private List<Item> setItemList() {
 		Vector<Item> result = new Vector<Item>();
 		Item a = new Item();
 		a.setPostDate("2018-11-29");
 		Item b = new Item();
 		b.setPostDate("2018-11-30");
 		
-		ItemList itemList = new ItemList(result);
-		return itemList;
+		return result;
 	}
 	
 	@Test
 	public void testAddItem() {
-		ItemList itemList = setItemList();
+		ItemList itemList = new ItemList(setItemList());
 		Item a = new Item();
 		a.setPostDate("2018-11-30");
 		itemList.addItem(a);
 		
-		assertEquals(itemList.getItem(2).getPostDate(), "11/30/2018");
+		assertEquals( itemList.getItem( itemList.getQuantity()-1 ).getPostDate(), "11/30/2018");
 	}
 	
 	@Test
 	public void testgetQuantity() {
-		ItemList itemList = setItemList();
-		assertEquals(itemList.getQuantity(), 2);
+		ItemList itemList = new ItemList(setItemList());
+		Item a = new Item();
+		itemList.addItem(a);
+		assertEquals(itemList.getQuantity(), 1);
 		ItemList itemList2 = new ItemList(null);
-		assertEquals(itemList.getQuantity(), 0);
+		assertEquals(itemList2.getQuantity(), 0);
 	}
 }
